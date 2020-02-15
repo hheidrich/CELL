aa510e07976c826724f7ff5bd59e980f6637e05e
Luca Rendsburg <lrendsburg@tml19b.Informatik.Uni-Tuebingen.De>
2020-02-15 16:46:15 +0100

    Added graph_from_scores and score_matrix_from_transition_matrix.

--- utils.py
+++ utils.py
@@ -98,3 +98,72 @@ def link_prediction_performance(scores_matrix, val_ones, val_zeros):
     edge_scores = np.append(scores_matrix[val_ones[:,0], val_ones[:,1]],
                             scores_matrix[val_zeros[:,0], val_zeros[:,1]])
     return roc_auc_score(actual_labels_val, edge_scores), average_precision_score(actual_labels_val, edge_scores)
+
+
+def scores_matrix_from_transition_matrix(transition_matrix, symmetric=True):
+    """
+    Compute the scores matrix from the transition matrix.
+
+    Args:
+        transition_matrix (np.array, shape=(N,N)): Matrix whose entries (i,j) correspond to the probability of a 
+                                                   transition from node i to j.
+        symmetric (bool, default:True): If True, symmetrize the resulting scores matrix.
+
+    Returns:
+        scores_matrix(sp.csr.csr_matrix, shape=(N, N)): Matrix whose entries (i,j) correspond to the weight of the 
+                                                        directed edge (i, j) in an edge-independent model.
+    """
+    
+    N = transition_matrix.shape[0]
+    p_stationary = np.real(eigs(transition_matrix.T, k=1, sigma=0.99999)[1])
+    p_stationary /= p_stationary.sum()
+    scores_matrix = np.maximum(p_stationary * transition_matrix, 0)
+    
+    if symmetric:
+        scores_matrix += scores_matrix.T
+    
+    return scores_matrix
+    
+    
+def graph_from_scores(scores_matrix, n_edges):
+    """
+    Assemble a symmetric binary graph from the input score matrix. Ensures that there will be no singleton nodes.
+    See the paper for details.
+
+    Args:
+        scores_matrix (sp.csr.csr_matrix, shape=(N, N)): Matrix whose entries (i,j) correspond to the weight of the 
+                                                        directed edge (i, j) in an edge-independent model.
+        n_edges (int): The desired number of edges in the generated graph.
+
+    Returns
+    -------
+    target_g (sp.csr.csr_matrix, shape=(N, N)): Adjacency matrix of the generated graph.
+    """
+
+    target_g = sp.csr_matrix(scores.shape)
+    
+    np.fill_diagonal(scores, 0)
+    
+    degrees = scores.sum(1)   # The row sum over the scores.
+
+    N = scores.shape[0]
+
+    for n in range(N): # Iterate over the nodes
+        target = np.random.choice(N, p=scores[n]/degrees[n])
+        target_g[n, target] = 1
+        target_g[target, n] = 1
+
+
+    diff = np.round((2 * n_edges - target_g.sum())/2)
+    if diff > 0:
+        triu = np.triu(scores)
+        triu[target_g.nonzero()] = 0
+        triu = triu / triu.sum()
+
+        triu_ixs = np.triu_indices_from(scores)
+        extra_edges = np.random.choice(triu_ixs[0].shape[0], replace=False, p=triu[triu_ixs], size=int(diff))
+
+        target_g[(triu_ixs[0][extra_edges], triu_ixs[1][extra_edges])] = 1
+        target_g[(triu_ixs[1][extra_edges], triu_ixs[0][extra_edges])] = 1
+
+    return target_g
