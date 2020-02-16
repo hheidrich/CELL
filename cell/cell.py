7d9fe594f36e5da1b1b5d794b080d45bfc7bbe26
hheidrich <holger.heidrich@uni-tuebingen.de>
2020-02-16 15:36:29 +0100

    Add some docstrings.

--- cell/cell.py
+++ cell/cell.py
@@ -8,8 +8,16 @@ DTYPE = torch.float32
 
 
 class Callback(abc.ABC):
+    """Abstract Class to customize train loops.
+
+    Applications include logging, printing or the implementation of stopping conditions.
+
+    Attributes:
+        invoke_every(int): The number of calls required to invoke the Callback once.
+    """
+    
     def __init__(self, invoke_every):
-        self.training_stopped = False
+        self._training_stopped = False
         self.invoke_every = invoke_every
         
     def __call__(self, loss, model):
@@ -17,25 +25,35 @@ class Callback(abc.ABC):
             self.invoke(loss, model)
         
     def stop_training(self):
-        self.training_stopped = True
+        self._training_stopped = True
     
     @abc.abstractmethod
-    def invoke(self, loss, model):
+    def invoke(self):
+        """This abstract method is intended to implement the behaviour of classes derived from Callback."""
         pass
 
 
 class EdgeOverlapCriterion(Callback):
+    """Tracks the edge overlap and stops the training if the limit is met.
+
+    Attributes:
+        invoke_every(int): The number of calls required to invoke the Callback once.
+        edge_overlap_limit(float): Stops the training if the models edge overlap reaches this limit.
     """
-    This callback serves in three ways:
-    - It tracks the EdgeOverlap and stops if the limit is met.
-    - It tracks the validation AUC-ROC score and the average precision.
-    - It tracks the total time.
-    """
-    def __init__(self, invoke_every, EO_limit=1.):
+    
+    def __init__(self, invoke_every, edge_overlap_limit=1.):
         super().__init__(invoke_every)
-        self.EO_limit = EO_limit
+        self.edge_overlap_limit = edge_overlap_limit
 
     def invoke(self, loss, model):
+        """Computes the edge overlap and prints the actual step, loss, edge overlap and total time.
+
+        It also stops the training if the computed edge overlap reaches self.edge_overlap_limit.
+
+        Args:
+            loss(float): The latest loss value.
+            model(Cell): The instance of the model being trained.
+        """
         start = time.time()
         model.update_scores_matrix()
         sampled_graph = model.sample_graph()
@@ -48,23 +66,42 @@ class EdgeOverlapCriterion(Callback):
               f'Loss: {loss:.5f}',
               f'Edge-Overlap: {overlap:.3f}',
               f'Total-Time: {int(model.total_time)}')
-        if overlap >= self.EO_limit:
+        if overlap >= self.edge_overlap_limit:
             self.stop_training()
 
 
 class LinkPredictionCriterion(Callback):
+    """Evaluates the link prediction performance and stops the training if there is no improvement for several steps.
+    
+    It ensures that the model's score_matrix is set to the score_matrix yielding the best results so far.
+    
+    Attributes:
+        invoke_every(int): The number of calls required to invoke the Callback once.
+        edge_overlap_limit(float): Stops the training if the models edge overlap reaches this limit.
+        val_ones(np.ndarray): Validation ones for link prediction.
+        val_zeros(np.ndarray): Validation zeros for link prediction.
+        max_patience(int): Maximal number of invokes without improvement of link prediction performance
+            until the training is stopped.
+    """
     def __init__(self, invoke_every, val_ones, val_zeros, max_patience):
         super().__init__(invoke_every)
         self.val_ones = val_ones
         self.val_zeros = val_zeros
         self.max_patience = max_patience
 
-        self.patience = 0
-        self.best_model = None
-        self.best_link_pred_score = 0.
+        self._patience = 0
+        self._best_scores_matrix = None
+        self._best_link_pred_score = 0.
 
     def invoke(self, loss, model):
-        """"""
+        """Evaluates the link prediction performance and prints the actual step, loss, edge overlap and total time.
+
+        It also stops the training if there is no improvement for self.max_patience invokes.
+
+        Args:
+            loss(float): The latest loss value.
+            model(Cell): The instance of the model being trained.
+        """
         start = time.time()
         model.update_scores_matrix()
         roc_auc, avg_prec = utils.link_prediction_performance(model._scores_matrix,
@@ -82,23 +119,28 @@ class LinkPredictionCriterion(Callback):
               f'Total-Time: {int(model.total_time)}')
         link_pred_score = roc_auc + avg_prec
         
-        if link_pred_score > self.best_link_pred_score:
-            self.best_link_pred_score = link_pred_score
-            self.best_scores_matrix = model._scores_matrix.copy()
-            self.patience = 0
+        if link_pred_score > self._best_link_pred_score:
+            self._best_link_pred_score = link_pred_score
+            self._best_scores_matrix = model._scores_matrix.copy()
+            self._patience = 0
             
-        elif self.patience >= self.max_patience:
+        elif self._patience >= self.max_patience:
             self.stop_training()
             
         else:
-            self.patience += 1
+            self._patience += 1
             
-        model._scores_matrix = self.best_scores_matrix
+        model._scores_matrix = self._best_scores_matrix
 
 
 class Cell(object):
+    """Implements the Cross Entropy Low-rank Logits graph generative model.
+    
+    Attributes:
+        
+    """
     def __init__(self, A, H, loss_fn=None, callbacks=[]):
-        self.num_edges = A.sum()/2
+        self.num_edges = A.sum() / 2
         self.A_sparse = A
         self.A = torch.tensor(A.toarray())
         self.step = 1
@@ -106,7 +148,7 @@ class Cell(object):
         self._optimizer = None
         
         N = A.shape[0]
-        gamma = np.sqrt(2/(N+H))
+        gamma = np.sqrt(2 / (N + H))
         self.W_down = (gamma * torch.randn(N, H, device=DEVICE, dtype=DTYPE)).clone().detach().requires_grad_()
         self.W_up = (gamma * torch.randn(H, N, device=DEVICE, dtype=DTYPE)).clone().detach().requires_grad_()
         
@@ -127,17 +169,14 @@ class Cell(object):
         return W
     
     def built_in_loss_fn(self, W, A, num_edges):
-        """
-        Computes the weighted cross-entropy loss in logits with weight matrix M * P.
-        Parameters
-        ----------
-        W: torch.tensor of shape (N, N)
-                Logits of learnable (low rank) transition matrix.
+        """Computes the weighted cross-entropy loss in logits with weight matrix M * P.
 
-        Returns
-        -------
-        loss: torch.tensor (float)
-                Loss at logits.
+        Args:
+            W(torch.tensor): Logits of learnable (low rank) transition matrix..
+            A(torch.tensor): The instance of the model being trained.
+            
+        Returns:
+            (torch.tensor): Loss at logits.
         """
         d = torch.log(torch.exp(W).sum(dim=-1, keepdims=True))
         loss = .5 * torch.sum(A * (d * torch.ones_like(A) - W)) / num_edges
@@ -167,7 +206,7 @@ class Cell(object):
             self.total_time += time
             for callback in self.callbacks:
                 callback(loss=loss, model=self)
-                stop = stop or callback.training_stopped    
+                stop = stop or callback._training_stopped    
             if stop: break
     
     def update_scores_matrix(self):
